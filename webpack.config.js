192c0f89cff07a9b807b137f6820fdc809698df8
Jan Kowalski <jan.kowalski@softwaremill.com>
2016-01-06 14:00:31 +0100

    uglify

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,11 @@
+var webpack = require('webpack');
+
 module.exports = {
   context: __dirname + '/app',
   entry: './index.js',
   output: {
     path: __dirname + '/app',
     filename: 'bundle.js'
-  }
+  },
+  plugins: [new webpack.optimize.UglifyJsPlugin()]
 };
