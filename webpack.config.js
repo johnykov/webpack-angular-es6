af38e8299be4e551161ed35affd097a343c43fad
Jan Kowalski <jan.kowalski@softwaremill.com>
2016-02-09 14:57:51 +0100

    complete 7 video, production, missing 6

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack');
 
-module.exports = {
+var config = {
   context: __dirname + '/app',
   entry: './index.js',
   output: {
@@ -21,3 +21,9 @@ module.exports = {
     ]
   }
 };
+
+if (process.env.NODE_ENV === 'production'){
+  config.output.path = __dirname + '/dist';
+}
+
+module.exports =config;
