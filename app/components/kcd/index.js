56d36f58093368a8cc1206ba7da643985e1796fa
Jan Kowalski <jan.kowalski@softwaremill.com>
2016-04-14 12:12:01 +0200

    require replace imports

--- app/components/kcd/index.js
+++ app/components/kcd/index.js
@@ -1,5 +1,3 @@
-import registerKcdHello from './kcd-hello'
-
 export default ngModule => {
-  registerKcdHello(ngModule)
+  require('./kcd-hello')(ngModule)
 };
--- app/features/main/index.js
+++ app/features/main/index.js
@@ -1,6 +1,3 @@
-import mainRouting from "./main.routes";
-
-//we are not using angular modules system, it's obsolete while having webpack
 export default ngModule => {
-  ngModule.config(mainRouting);
+  ngModule.config(require('./main.routes'));
 };
