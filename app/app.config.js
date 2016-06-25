58cd284b550b7d1fd1898352b2070cd36b6c3fd2
Jan Kowalski <hanskoff@gmail.com>
2016-06-25 23:02:42 +0200

    make ng-annotate plugin work

--- app/app.config.js
+++ app/app.config.js
@@ -1,6 +1,7 @@
-routing.$inject = ['$urlRouterProvider', '$locationProvider'];
+// routing.$inject = ['$urlRouterProvider', '$locationProvider'];
 
 export default function routing($urlRouterProvider, $locationProvider) {
+  "ngInject";
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
 }
--- app/features/home/home.routes.js
+++ app/features/home/home.routes.js
@@ -1,6 +1,7 @@
-homeRoutes.$inject = ['$stateProvider'];
+// homeRoutes.$inject = ['$stateProvider'];
 
 export default function homeRoutes($stateProvider) {
+  "ngInject";
   $stateProvider.state('home', {
       url: '/home', //TODO: mod rewrite server side, http://stackoverflow.com/questions/16569841/angularjs-html5-mode-reloading-the-page-gives-wrong-get-request
       template: require('./home.html'),
